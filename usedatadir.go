9edef639b0c099222d5d18ee15753f4d4d3bd1f2
Johan Lindh <johan@linkdata.se>
2024-06-12 12:29:46 +0200

    docs

--- usedatadir.go
+++ usedatadir.go
@@ -6,22 +6,23 @@ import (
 	"path/filepath"
 )
 
-// DefaultDataDir returns dataDir if not empty, otherwise the joined path
-// of the default user configuration and defaultsuffix.
-func DefaultDataDir(dataDir, defaultsuffix string) (string, error) {
+// DefaultDataDir returns dataDir if not empty, otherwise if
+// defaultSuffix is not empty it returns the joined path
+// of os.UserConfigDir() and defaultSuffix.
+func DefaultDataDir(dataDir, defaultSuffix string) (string, error) {
 	var err error
-	if dataDir == "" {
+	if dataDir == "" && defaultSuffix != "" {
 		dataDir, err = os.UserConfigDir()
 		if err == nil {
-			dataDir = path.Join(dataDir, defaultsuffix)
+			dataDir = path.Join(dataDir, defaultSuffix)
 		}
 	}
 	return dataDir, err
 }
 
 // UseDataDir expands environment variables in dataDir,
-// transforms it into an absolute path, creates it
-// if it does not exist and finally changes current directory
+// transforms it into an absolute path, creates it if
+// it does not exist and finally changes current directory
 // to that path.
 //
 // Returns the final path.
