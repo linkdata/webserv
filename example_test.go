bd620b3d34363a95ed260c88b68f80f3ac418691
Johan Lindh <johan@linkdata.se>
2024-06-12 13:36:45 +0200

    improve example

--- example_test.go
+++ example_test.go
@@ -10,24 +10,23 @@ import (
 var (
 	flagListen  = flag.String("listen", "", "serve HTTP requests on given [address][:port]")
 	flagCertDir = flag.String("certdir", "", "where to find fullchain.pem and privkey.pem")
-	flagUser    = flag.String("user", "", "switch to this user after startup (*nix only)")
-	flagDataDir = flag.String("datadir", "", "where to store data files after startup")
+	flagUser    = flag.String("user", "www-data", "switch to this user after startup (*nix only)")
+	flagDataDir = flag.String("datadir", "$HOME", "where to store data files after startup")
 )
 
 func Example() {
 	flag.Parse()
 
 	cfg := webserv.Config{
-		Listen:               *flagListen,
-		CertDir:              *flagCertDir,
-		User:                 *flagUser,
-		DataDir:              *flagDataDir,
-		DefaultDataDirSuffix: "webserv_example",
+		Listen:  *flagListen,
+		CertDir: *flagCertDir,
+		User:    *flagUser,
+		DataDir: *flagDataDir,
 	}
 
 	if l, err := cfg.Apply(log.Default()); err == nil {
 		defer l.Close()
-		log.Print("listening on", cfg.ListenURL)
+		log.Printf("listening on %q", cfg.ListenURL)
 	} else {
 		log.Fatal(err)
 	}
