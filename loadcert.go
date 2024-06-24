71e2728ddc09217a4c2370534a4a26c3016a85fc
Johan Lindh <johan@linkdata.se>
2024-06-24 11:19:08 +0200

    expand envvar in certDir

--- loadcert.go
+++ loadcert.go
@@ -2,12 +2,15 @@ package webserv
 
 import (
 	"crypto/tls"
+	"os"
 	"path"
 	"path/filepath"
 )
 
-// LoadCert does nothing if certDir is empty, otherwise tries to load a X509 key pair
-// from files named fullchainPem and privkeyPem in the given directory certDir.
+// LoadCert does nothing if certDir is empty, otherwise it expands
+// environment variables and transforms it into an absolute path.
+// It then tries to load a X509 key pair from the files named fullchainPem
+// and privkeyPem from the resulting directory.
 //
 // If fullchainPem is empty, it defaults to "fullchain.pem".
 // If privkeyPem is empty, it defaults to "privkey.pem".
@@ -15,6 +18,7 @@ import (
 // Return a non-nil cert and absolute path to certDir if there are no errors.
 func LoadCert(certDir, fullchainPem, privkeyPem string) (cert *tls.Certificate, absCertDir string, err error) {
 	if certDir != "" {
+		certDir = os.ExpandEnv(certDir)
 		if absCertDir, err = filepath.Abs(certDir); err == nil {
 			var cer tls.Certificate
 			if fullchainPem == "" {
